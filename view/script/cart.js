eb18041557130fc2378aa311d98575eef3c19d3f
tealerr <theetitle@gmail.com>
2023-11-13 23:36:53 +0700

    add cart count and total price, add checkout page and link value to  param url

---
+++ view/script/cart.js
@@ -0,0 +1,48 @@
+// Initialize cartCount and totalPrice with values from localStorage
+let cartCount = parseInt(localStorage.getItem("cartCount")) || 0;
+let totalPrice = parseFloat(localStorage.getItem("totalPrice")) || 0.0;
+
+// Update the HTML with initial values
+document.getElementById("cart-count").innerText = cartCount + " items";
+document.getElementById("total-price").innerText = totalPrice.toFixed(2);
+
+function addToCart(productPrice) {
+  cartCount++;
+  totalPrice += productPrice;
+
+  // Update HTML with new values
+  document.getElementById("cart-count").innerText = cartCount + " items";
+  document.getElementById("total-price").innerText = totalPrice.toFixed(2);
+
+  // Store values in localStorage
+  localStorage.setItem("cartCount", cartCount);
+  localStorage.setItem("totalPrice", totalPrice);
+}
+
+// function addToCart(productPrice) {
+//   // Retrieve current URL parameters
+//   const urlParams = new URLSearchParams(window.location.search);
+//   let cartCount = parseInt(urlParams.get("cartCount")) || 0;
+//   let totalPrice = parseFloat(urlParams.get("totalPrice")) || 0.0;
+
+//   // Update cart information
+//   cartCount++;
+//   totalPrice += productPrice;
+
+//   // Update HTML with new values
+//   document.getElementById("cart-count").innerText = cartCount + " items";
+//   document.getElementById("total-price").innerText = totalPrice.toFixed(2);
+
+//   // Update URL with new cart information
+//   urlParams.set("cartCount", cartCount);
+//   urlParams.set("totalPrice", totalPrice.toFixed(2));
+
+//   // Store values in localStorage
+//   localStorage.setItem("cartCount", cartCount);
+//   localStorage.setItem("totalPrice", totalPrice);
+
+//   // Update the link href and navigate to the new URL
+//   document.getElementById("cart-link").href =
+//     "checkout.html?" + urlParams.toString();
+//   window.location.href = document.getElementById("cart-link").href;
+// }
---
+++ view/script/checkout.js
@@ -0,0 +1,21 @@
+// Function to retrieve query parameters from the URL
+function getQueryParam(name) {
+  const urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get(name);
+}
+
+// Update cart count and total price based on query parameters
+document.addEventListener("DOMContentLoaded", function () {
+  const cartCount = getQueryParam("cartCount") || 0;
+  let totalPrice = parseFloat(getQueryParam("totalPrice")) || 0.0;
+
+  // Check if totalPrice is NaN, and set it to 0.0
+  if (isNaN(totalPrice)) {
+    totalPrice = 0.0;
+  }
+
+  // Update HTML with values
+  document.getElementById("cart-count").innerText = cartCount + " items";
+  document.getElementById("total-price").innerText =
+    "THB " + totalPrice.toFixed(2);
+});
